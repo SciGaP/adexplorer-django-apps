e8e4e70d433befd3d2df010c2c10f015cede0ebd
Marcus Christie <machristie@apache.org>
2021-09-20 18:42:55 -0400

    Rename django app name to pubad

--- pubad/apps.py
+++ pubad/apps.py
@@ -3,3 +3,22 @@ from django.apps import AppConfig
 
 class PubadConfig(AppConfig):
     name = 'pubad'
+    label = name
+    verbose_name = "PubAD"
+
+    # The following are Airavata Django Portal specific custom Django app settings
+
+    # Set url_home to a namespaced URL that will be the homepage when the custom
+    # app is selected from the main menu
+    url_home = "pubad:pubad_index"
+
+    # Set fa_icon_class to a FontAwesome CSS class for an icon to associate with
+    # the custom app. Find an icon class at https://fontawesome.com/icons?d=gallery&p=2&s=regular,solid&m=free
+    fa_icon_class = "fa-circle"
+
+    # Second level navigation. Defines sub-navigation that displays on the left
+    # hand side navigation bar in the Django Portal. This is optional but
+    # recommended if your custom Django app has multiple entry points. See the
+    # description of *nav* in
+    # https://apache-airavata-django-portal.readthedocs.io/en/latest/dev/new_django_app/#appconfig-settings
+    # for more details for more details.
