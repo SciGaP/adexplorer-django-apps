175c09665e09c5c80a7b4b1ad8255f2b889bb986
Marcus Christie <machristie@apache.org>
2022-01-28 17:19:56 -0500

    Adding real remove_space.py implementation

--- pubad/templatetags/remove_space.py
+++ pubad/templatetags/remove_space.py
@@ -1,7 +1,10 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='remove_space')
+@stringfilter
 def remove_space(value):
-    return value
+    """Removes all space of str, replace by _ """
+    return value.replace(' ', '_')
